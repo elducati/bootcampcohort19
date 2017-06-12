2fe453b2c953ff8aa5b8af6644c7f168a19966de
elducati <geomondi09@gmail.com>
2017-06-12 22:49:55 +0300

    testcase added

--- test_prime_number.py
+++ test_prime_number.py
@@ -15,6 +15,12 @@ class Prime_numberTest(unittest.TestCase):
 		expected = [2, 3, 5]
 		for i in expected:
 			self.assertIsInstance(i, int )
+	def test_prime_number_for_less_than_two(self):
+		result = prime_number(1)
+		self.assertFalse(result)
+	def test_for_negative_numbers(self):
+		result = prime_number(-5)
+		self.assertFalse(result)
 
 if __name__ == '__main__':
 	unittest.main()
