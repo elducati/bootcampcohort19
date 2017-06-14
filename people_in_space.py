87c1ac7a401b252f83185129f1e38cff27787a53
elducati <geomondi09@gmail.com>
2017-06-14 15:45:41 +0300

    print response from API end point

--- people_in_space.py
+++ people_in_space.py
@@ -5,5 +5,5 @@ response = requests.get("http://api.open-notify.org/astros.json")
 data = response.json()
 
 # 3 people are currently in space.
-print(data["number"]) #prints the number of people currently in space
-print(data) #prints the names
+print(data["number"])  # prints the number of people currently in space
+print(data)  # prints the names
