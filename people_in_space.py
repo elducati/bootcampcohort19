fef18a2c7633b445abc30994b981032746a2c694
elducati <geomondi09@gmail.com>
2017-06-16 02:15:06 +0300

    add function

--- people_in_space.py
+++ people_in_space.py
@@ -1,9 +1,11 @@
+import pprint
 import requests
 
-# Get the response from the API endpoint.
-response = requests.get("http://api.open-notify.org/astros.json")
-data = response.json()
-
-# 3 people are currently in space.
-print(data["number"])  # prints the number of people currently in space
-print(data)  # prints the names
+def space():
+    # Get the response from the API endpoint.
+    response = requests.get("http://api.open-notify.org/astros.json")
+    data = response.json()
+    # 3 people are currently in space.
+    print(data["number"]) # prints the number of people currently in space
+    print(pprint.pprint(data))  # prints the names
+print(space())
